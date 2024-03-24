aa05cbea7370702f506fcadb4d667914fa948b62
Oleg Klimenko <me@ctizen.dev>
2024-03-25 00:44:32 +0300

    remove yargs in favor of cli

--- dist/index.js
+++ dist/index.js
@@ -1,102 +1,63 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var tslib_1 = require("tslib");
+var cli = tslib_1.__importStar(require("cli"));
 var fs_1 = require("fs");
-var yargs_1 = tslib_1.__importStar(require("yargs"));
-var helpers_1 = require("yargs/helpers");
 var convert_1 = require("./src/convert");
 var get_stdin_1 = tslib_1.__importDefault(require("get-stdin"));
-(0, yargs_1.default)((0, helpers_1.hideBin)(process.argv)).command('pojson', 'i18n PO -> JSON converter', function (args) {
-    args
-        .option('src', {
-        alias: 's',
-        description: 'Define input JSON file name. Defaults to stdin.',
-        type: 'string',
-        default: '__stdin'
-    })
-        .option('output', {
-        alias: 'o',
-        description: 'Define output POT file name. If a file already ' +
-            'exists, it s contents will be overwritten. Defaults to stdout.',
-        type: 'string',
-        default: '__stdout'
-    }).option('withOccurences', {
-        alias: 'n',
-        description: 'Include occurences info into JSON file, '
-            + 'parsed from "#: ..." comments.',
-        type: 'boolean',
-        default: false
-    }).option('withComments', {
-        alias: 'c',
-        description: 'Include comments into JSON file, parsed '
-            + 'from "#. ..." comments.',
-        type: 'boolean',
-        default: false
-    }).option('withMeta', {
-        alias: 'm',
-        description: 'Include parsed PO header or plural form '
-            + 'into JSON file. Add all header values'
-            + 'without any params provided. Possable values "" | "full" | "plural"',
-        type: 'string',
-        default: undefined
-    }).option('prettify', {
-        alias: 'p',
-        description: 'Pretty-print JSON output.',
-        type: 'boolean',
-        default: false
-    }).option('help', {
-        alias: 'h',
-        description: 'Show this help',
-        type: 'boolean',
-        default: false
-    });
-}, function (_a) {
-    var help = _a.help, src = _a.src, output = _a.output, withOccurences = _a.withOccurences, withComments = _a.withComments, withMeta = _a.withMeta, prettify = _a.prettify;
-    if (help) {
-        (0, yargs_1.showHelp)();
-        process.exit(0);
+var options = cli.parse({
+    src: ['s', 'A source PO file to process', 'string', '__stdin'],
+    output: ['o', 'Output JSON file', 'string', '__stdout'],
+    withOccurences: ['n', 'Include occurences in JSON', 'bool', false],
+    withComments: ['c', 'Inclide comments in JSON', 'bool', false],
+    withMeta: ['m', 'Include parsed PO header into JSON', 'bool', false],
+    prettify: ['p', 'Prettify ouput JSON', 'bool', false],
+    help: ['h', 'Show some help', 'bool', false]
+});
+if (options.help) {
+    console.log("i18n PO -> JSON converter\n\nOptions:\n   -h / --help                   Show this help\n   -s / --src FILE               Define input PO file name. Defaults \n                                 to stdin.\n   -o / --output FILE            Define output JSON file name. If a file \n                                 already exists, it's contents will be\n                                 overwritten. Defaults to stdout.\n   -n / --withOccurences         Include occurences info into JSON file,\n                                 parsed from \"#: ...\" comments.\n   -c / --withComments           Include comments into JSON file, parsed\n                                 from \"#. ...\" comments.\n   -m / --withMeta               Include parsed PO header or plural form\n                                 into JSON file. Add all header values\n                                 without any params provided. Possible values: \"\" | \"full\" | \"plural\".\n   -p / --prettify               Pretty-print JSON output.\n");
+    process.exit(0);
+}
+console.warn('Running conversion for file: ', options.src);
+var parsedOptions = {
+    withOccurences: options.withOccurences,
+    withComments: options.withComments,
+    withMeta: false
+};
+if (options.withMeta === '' || options.withMeta === 'full') {
+    parsedOptions.withMeta = 'full';
+}
+else {
+    if (options.withMeta === 'plural') {
+        parsedOptions.withMeta = 'plural';
     }
-    console.warn('Running conversion for file: ', src);
-    var parsedOptions = {
-        withOccurences: withOccurences,
-        withComments: withComments,
-        withMeta: false
-    };
-    if (withMeta === '' || withMeta === 'full') {
-        parsedOptions.withMeta = 'full';
-    }
-    else {
-        if (withMeta === 'plural') {
-            parsedOptions.withMeta = 'plural';
+}
+if (options.src === '__stdin') {
+    (0, get_stdin_1.default)().then(function (data) {
+        try {
+            makeOutput((0, convert_1.convert)(data, parsedOptions), options.output, options.prettify);
         }
-    }
-    if (src === '__stdin') {
-        (0, get_stdin_1.default)().then(function (data) {
-            try {
-                makeOutput((0, convert_1.convert)(data, parsedOptions), output, prettify);
-            }
-            catch (e) {
-                console.error(e);
-                process.exit(1);
-            }
-        });
-    }
-    else {
-        (0, fs_1.readFile)(src, { encoding: 'utf-8' }, function (err, data) {
-            if (err) {
-                console.error(err);
-                process.exit(1);
-            }
-            try {
-                makeOutput((0, convert_1.convert)(data, parsedOptions), output, prettify);
-            }
-            catch (e) {
-                console.error(e);
-                process.exit(1);
-            }
-        });
-    }
-}).demandCommand().parse();
+        catch (e) {
+            console.error(e);
+            process.exit(1);
+        }
+    });
+}
+else {
+    (0, fs_1.readFile)(options.src, { encoding: 'utf-8' }, function (err, data) {
+        if (err) {
+            console.error(err);
+            process.exit(1);
+        }
+        try {
+            makeOutput((0, convert_1.convert)(data, parsedOptions), options.output, options.prettify);
+        }
+        catch (e) {
+            console.error(e);
+            process.exit(1);
+        }
+    });
+}
 function makeOutput(data, output, prettify) {
     if (output === '__stdout') {
         console.log(JSON.stringify(data, undefined, prettify ? '  ' : undefined));
--- index.ts
+++ index.ts
@@ -1,109 +1,81 @@
+import * as cli from 'cli';
 import { readFile, writeFile } from 'fs';
-import yargs, { Argv, ArgumentsCamelCase, showHelp } from 'yargs';
-import { hideBin } from 'yargs/helpers';
 import { convert } from './src/convert';
 import { TranslationJson } from 'i18n-proto';
 import { PoOptions } from './src/types';
 import getStdin from 'get-stdin'
 
-yargs(hideBin(process.argv)).command('pojson', 'i18n PO -> JSON converter', (args: Argv) => {
-  args
-    .option('src', {
-      alias: 's',
-      description: 'Define input JSON file name. Defaults to stdin.',
-      type: 'string',
-      default: '__stdin'
-    })
-    .option('output', {
-      alias: 'o',
-      description: 'Define output POT file name. If a file already ' +
-          'exists, it s contents will be overwritten. Defaults to stdout.',
-      type: 'string',
-      default: '__stdout'
-    }).option('withOccurences', {
-      alias: 'n',
-      description: 'Include occurences info into JSON file, '
-          + 'parsed from "#: ..." comments.',
-      type: 'boolean',
-      default: false
-    }).option('withComments', {
-      alias: 'c',
-      description: 'Include comments into JSON file, parsed '
-          + 'from "#. ..." comments.',
-      type: 'boolean',
-      default: false
-    }).option('withMeta', {
-      alias: 'm',
-      description: 'Include parsed PO header or plural form '
-          + 'into JSON file. Add all header values'
-          + 'without any params provided. Possable values "" | "full" | "plural"',
-      type: 'string',
-      default: undefined
-    }).option('prettify', {
-      alias: 'p',
-      description: 'Pretty-print JSON output.',
-      type: 'boolean',
-      default: false
-    }).option('help', {
-      alias: 'h',
-      description: 'Show this help',
-      type: 'boolean',
-      default: false
-    })
-  },
-  ({help, src, output, withOccurences, withComments, withMeta, prettify}: ArgumentsCamelCase<{
-    help: boolean;
-    src: string;
-    output: string;
-    withOccurences: boolean;
-    withComments: boolean;
-    withMeta: string;
-    prettify: boolean;
-  }>) => {
-    if (help) {
-      showHelp();
-      process.exit(0);
-    }
-    console.warn('Running conversion for file: ', src);
+const options = cli.parse({
+  src: ['s', 'A source PO file to process', 'string', '__stdin'],
+  output: ['o', 'Output JSON file', 'string', '__stdout'],
+  withOccurences: ['n', 'Include occurences in JSON', 'bool', false],
+  withComments: ['c', 'Inclide comments in JSON', 'bool', false],
+  withMeta: ['m', 'Include parsed PO header into JSON', 'bool', false],
+  prettify: ['p', 'Prettify ouput JSON', 'bool', false],
+  help: ['h', 'Show some help', 'bool', false]
+});
 
-    const parsedOptions: PoOptions = {
-      withOccurences,
-      withComments,
-      withMeta: false
-    }
+if (options.help) {
+  console.log(`i18n PO -> JSON converter
 
-    if (withMeta === '' || withMeta === 'full') {
-      parsedOptions.withMeta = 'full';
-    } else {
-      if (withMeta === 'plural') {
-        parsedOptions.withMeta = 'plural';
-      }
-    }
+Options:
+   -h / --help                   Show this help
+   -s / --src FILE               Define input PO file name. Defaults 
+                                 to stdin.
+   -o / --output FILE            Define output JSON file name. If a file 
+                                 already exists, it's contents will be
+                                 overwritten. Defaults to stdout.
+   -n / --withOccurences         Include occurences info into JSON file,
+                                 parsed from "#: ..." comments.
+   -c / --withComments           Include comments into JSON file, parsed
+                                 from "#. ..." comments.
+   -m / --withMeta               Include parsed PO header or plural form
+                                 into JSON file. Add all header values
+                                 without any params provided. Possible values: "" | "full" | "plural".
+   -p / --prettify               Pretty-print JSON output.
+`);
+  process.exit(0);
+}
 
-    if (src === '__stdin') {
-      getStdin().then((data) => {
-        try {
-          makeOutput(convert(data, parsedOptions), output, prettify);
-        } catch (e) {
-          console.error(e);
-          process.exit(1);
-        }
-      });
-    } else {
-      readFile(src, { encoding: 'utf-8' }, (err, data) => {
-        if (err) {
-          console.error(err);
-          process.exit(1);
-        }
-        try {
-          makeOutput(convert(data, parsedOptions), output, prettify);
-        } catch (e) {
-          console.error(e);
-          process.exit(1);
-        }
-      });
+console.warn('Running conversion for file: ', options.src);
+
+const parsedOptions: PoOptions = {
+  withOccurences: options.withOccurences,
+  withComments: options.withComments,
+  withMeta: false
+}
+
+if (options.withMeta === '' || options.withMeta === 'full') {
+  parsedOptions.withMeta = 'full';
+} else {
+  if (options.withMeta === 'plural') {
+    parsedOptions.withMeta = 'plural';
+  }
+}
+
+if (options.src === '__stdin') {
+  getStdin().then((data) => {
+    try {
+      makeOutput(convert(data, parsedOptions), options.output, options.prettify);
+    } catch (e) {
+      console.error(e);
+      process.exit(1);
     }
-  }).demandCommand().parse();
+  });
+} else {
+  readFile(options.src, { encoding: 'utf-8' }, (err, data) => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    try {
+      makeOutput(convert(data, parsedOptions), options.output, options.prettify);
+    } catch (e) {
+      console.error(e);
+      process.exit(1);
+    }
+  });
+}
 
 function makeOutput(data: TranslationJson, output: string, prettify: boolean) {
   if (output === '__stdout') {
